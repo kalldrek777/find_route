0e0a4465642554061030c51dced9756e7b938e68
aaa1 <kalldrek@yandex.ru>
2021-05-08 13:22:13 +0300

    Настройки для хероку

--- routes/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from routes.models import Route
-
-admin.site.register(Route)
--- trains/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrainsConfig(AppConfig):
-    name = 'trains'
